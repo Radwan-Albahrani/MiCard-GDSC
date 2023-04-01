6770e8fac3ebe00afe25146524fb9922b13185b3
Radwan-Albahrani <Radwan158@gmail.com>
2023-04-01 22:48:10 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.indigo,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage("assets/images/mainImage.png"),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: DetailsRow(
+                    text: "Radwan.Albahrani@gmail.com",
+                    icon: Icon(Icons.email),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Center(
+                  child: DetailsRow(
+                    text: "Radwan Albahrani",
+                    icon: Icon(Icons.person),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DetailsRow extends StatelessWidget {
+  const DetailsRow({
+    super.key,
+    required this.text,
+    required this.icon,
+  });
+
+  final String text;
+  final Icon icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        icon,
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.indigo,
+            fontSize: 20,
+            fontFamily: "Roboto",
+          ),
+        ),
+        const SizedBox(),
+      ],
+    );
+  }
+}
